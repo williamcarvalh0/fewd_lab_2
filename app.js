4fa05771aca73a0d5e16d0d861b5150e33e459ab
williamcarvalh0 <williamcarvalh0@hotmail.com>
2020-02-04 14:40:39 +0000

    commit

---
+++ app.js
@@ -0,0 +1,96 @@
+
+/* question 3*/
+var myAlphabet = ['A','B','C','D','E','F','G'];
+
+
+/* question 4*/
+
+var myAlphabetlength = function(){
+    return myAlphabet.length;
+};
+
+var result = myAlphabetlength();
+console.log(result);
+
+
+/* question 5*/
+var planets = ['1','2','3','4','5'];
+
+
+/* question 6*/
+console.log(planets);
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+
+/* question 7*/
+var wowDatatypes = ['William',30,true];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+
+/* question 8*/
+var x = 5, y = 10, z = 15;
+console.log('x + y = '+ (x + y));
+console.log('y - x = '+ (y - x));
+console.log('x * y = '+ (x * y));
+console.log('y / x = '+ (y / x));
+console.log('x % 2 = '+ (x % 2));
+console.log('x++ = '+ (x++));
+console.log('x-- = '+ (x--));
+
+
+/* question 9*/
+var a = 5, b = 'Hello', c = 'World!', d = 10;
+console.log('a + b = '+ (a + b));
+console.log('b + c = '+ (b + c));
+console.log('a + d = '+ (a + d));
+
+
+/* question 10*/
+var a = 5, b = 10, c = "5";
+var x = a;
+
+console.log('a == c = '+ (a == c));
+console.log('a === c = '+ (a === c));
+console.log('a == x = '+ (a == x));
+console.log('a != b = '+ (a != b));
+console.log('a > b = '+ (a > b));
+console.log('a < b = '+ (a < b));
+console.log('a >= b = '+ (a >= b));
+console.log('a <= b = '+ (a <= b));
+console.log('a >= c = '+ (a >= c));
+console.log('a <= c = '+ (a >= c));
+
+
+/* question 11*/
+var color = function(){
+    return 'red';
+};
+
+var result = color();
+console.log(result);
+
+
+var hello = function(){
+    return 'Hello world!';
+};
+
+
+/* question 12*/
+var result = hello();
+console.log(result);
+
+
+/* question 13*/
+var hello = function(){
+    return 'Hi!';
+};
+
+var x = hello();
+console.log(x);
+
